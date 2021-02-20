0c6421840c824c15a4a8e952349f2cf69ee07bcc
Rohit Parihar <59642321+rohitparihar-code@users.noreply.github.com>
2021-02-20 11:43:15 +0530

    Add files via upload

---
+++ A1_Q3(a).asm
@@ -0,0 +1,30 @@
+	.data
+nValue: .asciz "N = "
+sumValue: .asciz "\nSum: "
+
+	.text
+main:
+	li	t0, 10 # Arbitrary value of N
+	li	t1, 0 # sum variable initiated to zero
+	
+	la	a0, nValue
+	li	a7, 4
+	ecall # Prints "N = "
+	
+	add	a0, x0, t0 # Copy value of N to a0 to print it
+	li	a7, 1
+	ecall # Prints N value
+	
+	la	a0, sumValue
+	li	a7, 4
+	ecall # Prints "Sum = "
+	
+loop:
+	add	t1, t1, t0 # sum = sum + N
+	addi	t0, t0, -1 # Decrease value of N by 1
+	bgtz	t0, loop
+	
+exit:
+	add	a0, x0, t1
+	li	a7, 1
+	ecall	# Prints sum value
---
+++ A1_Q3(b).asm
@@ -0,0 +1,28 @@
+# Print sum of N numbers, N is input
+	.data
+inputN: .asciz "Enter the value of N: "
+output: .asciz "\nSum: "
+
+	.text
+main :
+	la	a0, inputN
+	li	a7, 4 #printInt
+	ecall
+	li	a7, 5 #readInt
+	ecall # Tends to receive input from user
+	
+	add	t0, x0, a0 # Copy the received input to t0 (N)
+	li	t1, 0 #initialize sum variable to zero
+	
+loop:
+	add	t1, t1, t0 # Sum = sum + N
+	addi	t0, t0, -1 # N = N -1
+	bgtz	t0, loop # While (N > 0)
+	
+exit:
+	la	a0, output
+	li	a7, 4 # printStr
+	ecall
+	add	a0, x0, t1
+	li	a7, 1 # printInt
+	ecall # Prints the value of sum
